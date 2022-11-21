acd46b6afd6aa8c38b3dd0abcf2686d484c04518
sijabulisiwe <75529225+sijabulisiwe@users.noreply.github.com>
2022-11-21 15:14:51 +0800

    Add files via upload

---
+++ listlargestnumberandsmallest.py
@@ -0,0 +1,8 @@
+# Write a Python program to get the largest number and smallest number from a list.
+x=[1 , 2 , 3 , 4 , 11 , 6 , 7 , 8 , 9]
+print("largest",max(x))
+print('Smallest',min(x))
+
+
+    
+    
